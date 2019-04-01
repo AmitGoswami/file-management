3602b06c3cdf4b7b0b42afd09c9d50446ba791e7
AmitGoswami <amit.gosu@gmail.com>
2019-04-01 14:10:24 +0530

    renaming srt files to videos files name

--- file-util-v1.js
+++ file-util-v1.js
@@ -28,5 +28,47 @@ var listFile = function(dirname, fileMapList) {
   fileMapList.push(fileMap);
   return fileMapList;
 }
-
-exports.listFile = listFile;
+var renameSRTtoVideoFileName = function(dirname) {
+  var videoFileNameFormat = {
+    serialName: '1,2',
+    episodeNumber: '3',
+    episodeName: '4',
+    nameSeparator: ' '
+  };
+  const subtitleFile = 'srt';
+  var listOfFiles = fs.readdirSync(dirname);
+  listOfFiles.forEach(file => {
+    var extn = file.substring(file.lastIndexOf('.') + 1);
+    if (!file.includes(subtitleFile)) {
+      file = file.substring(0, file.lastIndexOf('.'));
+      var episodeName = file.split(videoFileNameFormat.nameSeparator)[videoFileNameFormat.episodeName - 1];
+      var fileWithextn = fileWithExtn(dirname, subtitleFile);
+      fileWithextn.forEach(filename => {
+        if (filename.includes(episodeName)) {
+          var newFileName = file.substring(file.lastIndexOf('.') + 1) + '.' + subtitleFile;
+          if (newFileName != filename) {
+            console.log(`renaming file "${filename}" to "${newFileName}"`);
+            fs.rename(dirname + '\\' + filename, dirname + '\\' + newFileName, function(err) {
+              if (err) {
+                console.log(err);
+              }
+            });
+          }
+        }
+      })
+    }
+  });
+}
+var fileWithExtn = function(dirname, _extn) {
+  var listOfFiles = fs.readdirSync(dirname);
+  var extnFileList = [];
+  listOfFiles.forEach(file => {
+    var extn = file.substring(file.lastIndexOf('.') + 1);
+    if (extn == _extn) {
+      extnFileList.push(file);
+    }
+  });
+  return extnFileList;
+}
+renameSRTtoVideoFileName("C:\\Users\\amigo\\Downloads\\12 Monkeys Season 3 Complete 720p HDTV x264 [i_c]")
+//exports.listFile = listFile;
